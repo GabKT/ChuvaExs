3526917c0fdc43a3e4bf5e9791cf4c16d14fb5db
Gabriel Barros <ktbarros07@gmail.com>
2024-04-14 20:39:31 -0300

    Trying to Fix

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -15,26 +15,22 @@ class Main {
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-
-    $data = (new Scrapper())->scrap($dom);
-
+    $payload = (new Scrapper())->scrap($dom);
     $writer = WriterEntityFactory::createXLSXWriter();
-    $writer->openToFile(__DIR__ .'/../../assets/model.xlsx');
-
-    
-    foreach($data as $data){
+    $writer->openToFile(__DIR__ . '/../../assets/model.xlsx');
+    foreach ($payload as $data) {
       $cells = [];
-      $cells[] = WriterEntityFactory::createCell((string)$data->id);
-      $cells[] = WriterEntityFactory::createCell((string)$data->title);
-      $cells[] = WriterEntityFactory::createCell((string)$data->type);
-      foreach($data->authors as $author){
-        $cells[] = WriterEntityFactory::createCell((string)$author->name);
-        $cells[] = WriterEntityFactory::createCell((string)$author->institution);
+      $cells[] = WriterEntityFactory::createCell((string) $data->id);
+      $cells[] = WriterEntityFactory::createCell((string) $data->title);
+      $cells[] = WriterEntityFactory::createCell((string) $data->type);
+      foreach ($data->authors as $author) {
+        $cells[] = WriterEntityFactory::createCell((string) $author->name);
+        $cells[] = WriterEntityFactory::createCell((string) $author->institution);
       }
       $row = WriterEntityFactory::createRow($cells);
       $writer->addRow($row);
     }
     $writer->close();
   }
+
 }
-?>
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -21,41 +21,35 @@ class Scrapper {
       //a[@class='paper-card p-lg bd-gradient-left']//div//div[@class='tags mr-sm'] | 
       //div[@class='volume-info']");
     $allDatas = [];
-    $count = 0; //wait for all variables to be filled
-
-    foreach($dataList as $data){
-      if($data->nodeName === 'h4'){
+    $count = 0; 
+    foreach ($dataList as $data) {
+      if ($data->nodeName === 'h4'){
           $title = $data->nodeValue;
-      }
-      if ($data->nodeName === 'div' && $data->getAttribute('class')==='authors') {
+    }
+    if ($data->nodeName === 'div' && $data->getAttribute('class') === 'authors') {
           $spans = $data->getElementsByTagName('span');
-          foreach($spans as $span){
+          foreach ($spans as $span) {
             $dep = $span->getAttribute('title');
             $name = $span->nodeValue;
             $author = new Person($name, $dep);
-            $authors[]=$author;
+            $authors[] = $author;
           }
-      }
-      if($data->nodeName === 'div' && $data->getAttribute('class')==='tags mr-sm'){
+    }
+    if ($data->nodeName === 'div' && $data->getAttribute('class') === 'tags mr-sm') {
           $type = $data->nodeValue;
-      } 
-      if($data->nodeName === 'div' && $data->getAttribute('class')==='volume-info'){
+    }
+    if($data->nodeName === 'div' && $data->getAttribute('class') === 'volume-info'){
           $id = $data->nodeValue;
-      } 
-      $count++;
-      if($count == 4){
+    }
+    $count++;
+    if($count == 4){
         $paper = new Paper($id, $title, $type, $authors);
         $allDatas[] = $paper;
-        $count=0; // reset count
-        $authors = []; //reset array
-      }
+        $count=0; // Reset count.
+        $authors = []; // Reset array.
     }
-    //var_dump($allDatas);
+    }
     return $allDatas;
-    /*capturar dados dom e sair preenchendo as variaveis por verificaçao -> gerou varios colaterais como dados fora de ordem ou com dados vazios, cheguei a implementar soluçoes 
-    para esses problemas mas sempre apareciam mais foi ai que eu parei tudo pra ter uma outra estrategia ja que os dados estavam sendo coletados corretamente entao o erro nao era
-    a captura e sim como eu lidava com eles foi ai que eu comecei a debugar variavel por variavel e funçao por funçao e percebi que os dados que eu precisava vinham de 4 em 4
-    como eu defini na query do xpath e um simples contador ja resolvia meu problema */
   }
+
 }
-?>
