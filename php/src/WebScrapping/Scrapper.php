7ad32ccd0e556eeb38f64762f5dba151057eab99
Gabriel Barros <ktbarros07@gmail.com>
2024-04-14 01:29:51 -0300

    Bug fixes

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -21,41 +21,42 @@ class Scrapper {
       //a[@class='paper-card p-lg bd-gradient-left']//div//div[@class='tags mr-sm'] | 
       //div[@class='volume-info']");
     $allDatas = [];
+    $count = 0;
 
     foreach($dataList as $data){
-
       if($data->nodeName === 'h4'){
-        $title = $data->nodeValue;
+          $title = $data->nodeValue;
       }
-      $authors = [];
+        
       if ($data->nodeName === 'div' && $data->getAttribute('class')==='authors') {
-        $spans = $data->getElementsByTagName('span');
-        foreach($spans as $span){
-          $dep = $span->getAttribute('title');
-          $name = $span->nodeValue;
-          $author = new Person($dep, $name);
-          $authors[]=$author;
-        }
+          $spans = $data->getElementsByTagName('span');
+          foreach($spans as $span){
+            $dep = $span->getAttribute('title');
+            $name = $span->nodeValue;
+            $author = new Person($name, $dep);
+            $authors[]=$author;
+          }
       }
       if($data->nodeName === 'div' && $data->getAttribute('class')==='tags mr-sm'){
-        $type = $data->nodeValue;
+          $type = $data->nodeValue;
       } 
       if($data->nodeName === 'div' && $data->getAttribute('class')==='volume-info'){
-        $id = $data->nodeValue;
+          $id = $data->nodeValue;
       } 
-      $paper = new Paper($id, $title, $type, $authors);
-      $flag = 0;
-      foreach($allDatas as $data){
-        if($paper->title == $data->title){
-          $flag = 1;
-        }
-      }
-      if($flag != 1 && !empty($authors)){
+      $count++;
+      if($count == 4){
+        $paper = new Paper($id, $title, $type, $authors);
         $allDatas[] = $paper;
+        $count=0;
+        $authors = [];
       }
     }
+    //var_dump($allDatas);
     return $allDatas;
+    /*capturar dados dom e sair preenchendo as variaveis por verificaçao -> gerou varios colaterais como dados fora de ordem ou com dados vazios, cheguei a implementar soluçoes 
+    para esses problemas mas sempre apareciam mais foi ai que eu parei tudo pra ter uma outra estrategia ja que os dados estavam sendo coletados corretamente entao o erro nao era
+    a captura e sim como eu lidava com eles foi ai que eu comecei a debugar variavel por variavel e funçao por funçao e percebi que os dados que eu precisava vinham de 4 em 4
+    como eu defini na query do xpath e um simples contador ja resolvia meu problema */
   }
-
 }
 ?>
