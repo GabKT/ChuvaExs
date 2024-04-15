71cded5fd7fc881f428a3d45e0d868747c9f332e
Gabriel Barros <ktbarros07@gmail.com>
2024-04-14 21:57:53 -0300

    First Row Add

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -13,11 +13,23 @@ class Main {
    * Main runner, instantiates a Scrapper and runs.
    */
   public static function run(): void {
+
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
     $payload = (new Scrapper())->scrap($dom);
     $writer = WriterEntityFactory::createXLSXWriter();
     $writer->openToFile(__DIR__ . '/../../assets/model.xlsx');
+    $columns = ['ID', 'TITLE', 'TYPE'];
+    for ($i = 1; $i < 17; $i++) {
+      $columns[] = 'AUTHOR ' . $i;
+      $columns[] = 'AUTHOR ' . $i . " INSTITUTION";
+    }
+    $cell = [];
+    foreach ($columns as $column) {
+      $cell[] = WriterEntityFactory::createCell($column);
+    }
+    $firstRow = WriterEntityFactory::createRow($cell);
+    $writer->addRow($firstRow);
     foreach ($payload as $data) {
       $cells = [];
       $cells[] = WriterEntityFactory::createCell((string) $data->id);
